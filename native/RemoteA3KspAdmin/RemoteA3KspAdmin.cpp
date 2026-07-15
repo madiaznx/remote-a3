6b5951d4d988b0019c56b5b9e5f44555ec10975e
madia <115204437+madiaznx@users.noreply.github.com>
2026-07-15 14:18:09 -0300

    Update native/RemoteA3KspAdmin/RemoteA3KspAdmin.cpp for v0.3.4

--- native/RemoteA3KspAdmin/RemoteA3KspAdmin.cpp
+++ native/RemoteA3KspAdmin/RemoteA3KspAdmin.cpp
@@ -5,7 +5,7 @@
 #pragma comment(lib, "bcrypt.lib")
 
 #ifndef NCRYPT_INTERFACE
-#define NCRYPT_INTERFACE BCRYPT_SIGNATURE_INTERFACE
+#define NCRYPT_INTERFACE 0x00010001
 #endif
 
 typedef NTSTATUS(NTAPI* BCryptRegisterProviderPtr)(LPCWSTR, ULONG, PCRYPT_PROVIDER_REG);
@@ -46,6 +46,28 @@ struct BCryptApi {
 static constexpr const wchar_t* kProviderName = L"Remote A3 Key Storage Provider";
 static constexpr const wchar_t* kDllName = L"RemoteA3Ksp.dll";
 
+static std::wstring GetExecutableDirectory()
+{
+    wchar_t path[MAX_PATH]{};
+    DWORD chars = GetModuleFileNameW(nullptr, path, ARRAYSIZE(path));
+    if (chars == 0 || chars >= ARRAYSIZE(path)) {
+        return L".";
+    }
+
+    std::wstring value(path);
+    size_t slash = value.find_last_of(L"\\/");
+    if (slash == std::wstring::npos) {
+        return L".";
+    }
+
+    return value.substr(0, slash);
+}
+
+static std::wstring GetDefaultDllPath()
+{
+    return GetExecutableDirectory() + L"\\" + kDllName;
+}
+
 static void PrintStatus(const wchar_t* operation, NTSTATUS status)
 {
     if (status == 0) {
@@ -56,7 +78,7 @@ static void PrintStatus(const wchar_t* operation, NTSTATUS status)
     std::wcout << operation << L": 0x" << std::hex << status << std::dec << std::endl;
 }
 
-static int RegisterProvider()
+static int RegisterProvider(const std::wstring& dllPath)
 {
     BCryptApi api;
     if (!api.Load()) {
@@ -78,7 +100,7 @@ static int RegisterProvider()
     PCRYPT_INTERFACE_REG interfaces[] = { &interfaceReg };
 
     CRYPT_IMAGE_REG userModeImage{};
-    userModeImage.pszImage = const_cast<PWSTR>(kDllName);
+    userModeImage.pszImage = const_cast<PWSTR>(dllPath.c_str());
     userModeImage.cInterfaces = ARRAYSIZE(interfaces);
     userModeImage.rgpInterfaces = interfaces;
 
@@ -142,12 +164,14 @@ static int UnregisterProvider()
 int wmain(int argc, wchar_t** argv)
 {
     if (argc < 2) {
-        std::wcerr << L"Usage: RemoteA3KspAdmin.exe register|unregister" << std::endl;
+        std::wcerr << L"Usage: RemoteA3KspAdmin.exe register [RemoteA3Ksp.dll path]|unregister" << std::endl;
         return 2;
     }
 
     if (_wcsicmp(argv[1], L"register") == 0) {
-        return RegisterProvider();
+        std::wstring dllPath = argc >= 3 ? argv[2] : GetDefaultDllPath();
+        std::wcout << L"DLL: " << dllPath << std::endl;
+        return RegisterProvider(dllPath);
     }
 
     if (_wcsicmp(argv[1], L"unregister") == 0) {
