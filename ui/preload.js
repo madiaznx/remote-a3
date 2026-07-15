db80c4b31d5d3c34cf3ba7b0d9076c77f445b72c
madia <115204437+madiaznx@users.noreply.github.com>
2026-07-15 16:26:23 -0300

    Release 0.5.0 manual import UI

--- ui/preload.js
+++ ui/preload.js
@@ -2,6 +2,8 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("remoteA3", {
   status: () => ipcRenderer.invoke("remote-a3:status"),
+  available: () => ipcRenderer.invoke("remote-a3:available"),
+  importCertificate: (certificate) => ipcRenderer.invoke("remote-a3:import", certificate),
   setup: () => ipcRenderer.invoke("remote-a3:setup"),
   openPath: (targetPath) => ipcRenderer.invoke("remote-a3:open-path", targetPath)
 });
